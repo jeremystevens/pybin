e1f4282d14119d8f4ff7f19982ead5d85c02632d
Jeremy Stevens <jeremiahstevens@gmail.com>
2022-01-18 02:38:36 -0800

    updates to sqlalchemy in main.py

--- data/db.py
+++ data/db.py
@@ -10,7 +10,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 def get_connection():
     # added to prevent error / http://shorturl.at/hyT02
     base_dir = os.path.dirname(os.path.abspath(__file__))
-    db_path = os.path.join(base_dir, "paste.db")
+    db_path = os.path.join(base_dir, "pybin.db")
     connection = sqlite3.connect(db_path)
     return connection
 
@@ -22,15 +22,3 @@ def generate_random_id(min_size, max_size):
     numbers = string.digits
     random_string = rstr.rstr(lower_letters + letters + numbers, min_size, max_size)
     return random_string
-
-
-def insert_paste(paste_text, paste_syntax, paste_exp, paste_exposure, paste_name):
-    random_id = generate_random_id(6, 8)
-    # TODO Fix the Code below
-    # ALL THE CODE BELOW DOES NOT WORK
-    conn = get_connection()
-    cursor = conn.cursor()
-    cursor.execute('''INSERT INTO public_post(
-   postid,post_syntax,exp_int, post_exp,post_text, post_title) VALUES 
-   (''', random_id, paste_syntax, paste_exp, paste_exposure, paste_name, paste_text, '),''')
-    conn.commit()
--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import json
 import string
+from datetime import datetime
+
 from flask import Flask, render_template, request, url_for, redirect, flash, session
 from werkzeug.security import check_password_hash
 from flask_sqlalchemy import SQLAlchemy
-from data.db import get_connection, insert_paste
+from data.db import get_connection, generate_random_id
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nots0s3cr3t'
@@ -40,12 +42,48 @@ def submit_paste():
         paste_exp = request.form['paste_exp']
         paste_exposure = request.form['exposure']
         paste_name = request.form['paste_title']
-        # run the db function to enter data
-        insert_paste(paste_text, paste_syntax, paste_exp, paste_exposure, paste_name)
-        return redirect(url_for('index'))
+        print(paste_text)
+        # generate random id 
+        random_id = generate_random_id(6, 8)
+        pid = Post(post_id=random_id)
+        db.session.add(pid)
+        post_syn = Post(post_syntax=paste_syntax)
+        db.session.add(post_syn)
+        post_title = Post(post_title=paste_name)
+        db.session.add(post_title)
+        post_txt = Post(post_text=paste_text)
+        db.session.add(post_txt)
+        expr = Post(expiration=paste_exp)
+        db.session.add(expr)
+        expo = Post(exposure=paste_exposure)
+        db.session.add(expo)
+        date = datetime.now()
+        p_date = Post(post_date=date)
+        db.session.add(p_date)
+        # update to get size later
+        size_bt = 0
+        size = Post(post_size=size_bt)
+        db.session.add(size)
+        # set hits to zero
+        hits_count = 0
+        p_hits = Post(post_hits=hits_count)
+        db.session.add(p_hits)
+        db.session.commit()
+        # Redirect after post is complete Needs Work
+        # TODO fix this to redirect to view post page
+        # return render_template("view.html",
+        #       post_id=random_id)
+        return redirect(url_for('/'))
     else:
-        # TODO change this to redirect to post
         return redirect(url_for('index'))
+
+
+@app.route('/view/<post_id>')
+def get_post(post_id):
+    post = Post()
+    results = post.query.filter_by(post_id=post_id).first().post_title
+    print(results)
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
