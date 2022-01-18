4485707c5f544bdf75ad7085e6837b2669893da1
Jeremy Stevens <jeremiahstevens@gmail.com>
2022-01-18 00:48:11 -0800

    added flask sqlalchemy

--- main.py
+++ main.py
@@ -2,11 +2,28 @@ import json
 import string
 from flask import Flask, render_template, request, url_for, redirect, flash, session
 from werkzeug.security import check_password_hash
-
+from flask_sqlalchemy import SQLAlchemy
 from data.db import get_connection, insert_paste
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'nots0s3cr3t'
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///pybin.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# db model
+class Post(db.Model):
+    pid = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    post_id = db.Column(db.String(80), unique=True, nullable=False)
+    post_syntax = db.Column(db.String(80), nullable=False)
+    post_title = db.Column(db.String(200), nullable=False)
+    post_text = db.Column(db.String(8000), nullable=False)
+    expiration = db.Column(db.String(200), nullable=False)
+    exposure = db.Column(db.Integer, nullable=False)
+    post_date = db.Column(db.String(200), nullable=False)
+    post_size = db.Column(db.String(800))
+    post_hits = db.Column(db.String(8000))
 
 
 @app.route('/')
