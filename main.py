a8999471d929b7790e315acccde738bae7334f50
Jeremy Stevens <jeremiahstevens@gmail.com>
2022-01-19 18:59:08 -0800

    Dev-0.3 Updates

--- main.py
+++ main.py
@@ -61,13 +61,24 @@ def submit_paste():
         return redirect(url_for('get_post', random_id=random_id))
 
 
-@app.route('/view/<random_id>')
+@app.route('/p/<random_id>')
 def get_post(random_id):
     post = Post()
-    results = post.query.filter_by(post_id=random_id).first().post_title
-    print(results)
-    # change this to the view post view later.
-    return render_template('index.html')
+    post_id = post.query.filter_by(post_id=random_id).first().post_id
+    post_title = post.query.filter_by(post_id=random_id).first().post_title
+    post_syntax = post.query.filter_by(post_id=random_id).first().post_syntax
+    post_date = post.query.filter_by(post_id=random_id).first().post_date
+    # convert date to human Readable(works)
+    p_date = datetime.strptime(post_date, '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
+    post_size = post.query.filter_by(post_id=random_id).first().post_size
+    post_hits = post.query.filter_by(post_id=random_id).first().post_hits
+    # insert code here to update post hits.
+    post_expire = post.query.filter_by(post_id=random_id).first().expiration
+    exp_date = datetime.strptime(post_expire, '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
+    post_text = post.query.filter_by(post_id=random_id).first().post_text
+    return render_template('view.html', post_id=post_id, post_title=post_title, post_syntax=post_syntax,
+                           post_date=p_date, post_size=post_size, post_hits=post_hits, post_expire=post_expire,
+                           post_text=post_text)
 
 
 if __name__ == '__main__':
