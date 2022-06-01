aefae8a19b81711b28e7860530e61805fb28adfe
jeremystevens <jeremiahstevens@gmail.com>
2022-05-31 22:39:23 -0700

    just some housekeeping

--- data/db.py
+++ data/db.py
@@ -1,10 +1,28 @@
 """ db.py: - some tools to handle data before it is inserted into the DB. """
 
-__author__ = "Jeremy Stevens"
-__license__ = "GPL"
-__version__ = "1.0.0"
-__maintainer__ = "Jeremy Stevens"
-__status__ = "Development"
+# Copyright 2022 Jeremy Stevens <jeremiahstevens@gmail.com>
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+__version__ = '1.0.0'
+
+# ============================================================
 
 import sqlite3
 import os.path
