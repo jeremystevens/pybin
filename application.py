951ea5f3691bf6cc577e1f93e94dd44bfcf35faf
jeremystevens <jeremiahstevens@gmail.com>
2022-06-13 18:48:08 -0700

    closed #47 & Updated changelog with updates

--- application.py
+++ application.py
@@ -626,64 +626,61 @@ def profile(username):
         user_name = "Anonymous"
     # get user profile
     users = Users()
-    # check if the username is valid
-    try:
-        user_id = users.query.filter_by(username=username).first().user_id
-    except AttributeError:
-        return render_template('404.html', date=datetime.now())
     # get user post
     post = Post()
     profile = Profile()
-    # added this to fix issue #46 - profile not found
+    # added this to fix issue #47 - profile not found (prevent attribute error) added in V.1.1.6-bug-fix
     try:
         user_post = post.query.filter_by(poster=username).all()
+        # get post dates
+        post_date = post.query.with_entities(Post.post_date).all()
+        # users location
+        user_location = profile.query.filter_by(username=username).first().location
+        if user_location == None:
+            user_location = "N/A"
+        else:
+            user_location = user_location
+        # users post count
+        user_post_count = post.query.filter_by(poster=username).count()
+        # total views of user
+        user_total_views = profile.query.filter_by(username=username).first().total_views
+        if user_total_views == None:
+            user_total_views = 0
+        else:
+            user_total_views = user_total_views
+        # post count by user
+        user_post_count = post.query.filter_by(poster=username).count()
+        if user_post_count == None:
+            user_post_count = 0
+        else:
+            user_post_count = user_post_count
+        # last login time
+        user_last_login = profile.query.filter_by(username=username).first().last_login
+        # convert to string format
+        user_last_login = str(user_last_login)
+        # convert to human readable
+        user_last_login = datetime.strptime(user_last_login, '%Y-%m-%d %H:%M:%f').strftime('%m/%d/%Y')
+        # join_date
+        user_join_date = profile.query.filter_by(username=username).first().join_date
+        user_join_date = str(user_join_date)
+        # convert to human readable
+        # convert to %m/%d/%Y
+        user_join_date = datetime.strptime(user_join_date, '%Y-%m-%d %H:%M:%f').strftime('%m/%d/%Y')
+        # format post Dates to human readable
+        for i in range(len(post_date)):
+            post_date[i] = datetime.strptime(post_date[i][0], '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
+            # post_date[i] = datetime.strptime(post_date[i][0], '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
+            p_date = post_date[i]
+            # p_date = datetime.strptime(post_date[i], '%Y-%m-%d').strftime('%m/%d/%Y')
+        # return user profile
+        return render_template('profile.html', user_name=user_name, username=username, user_location=user_location,
+                               user_post_count=user_post_count, user_total_views=user_total_views,
+                               posts=user_post, user_last_login=user_last_login, date=p_date,
+                               user_join_date=user_join_date)
     except AttributeError:
         return render_template('404.html', date=datetime.now())
-    # if user is not found then redirect to profile not found page
-    # get post dates
-    post_date = post.query.with_entities(Post.post_date).all()
-    # users location
-    user_location = profile.query.filter_by(username=username).first().location
-    if user_location == None:
-        user_location = "N/A"
-    else:
-        user_location = user_location
-    # users post count
-    user_post_count = post.query.filter_by(poster=username).count()
-    # total views of user
-    user_total_views = profile.query.filter_by(username=username).first().total_views
-    if user_total_views == None:
-        user_total_views = 0
-    else:
-         user_total_views = user_total_views
-    # post count by user
-    user_post_count = post.query.filter_by(poster=username).count()
-    if user_post_count == None:
-        user_post_count = 0
-    else:
-        user_post_count = user_post_count
-    # last login time
-    user_last_login = profile.query.filter_by(username=username).first().last_login
-    # convert to string format
-    user_last_login = str(user_last_login)
-    # convert to human readable
-    user_last_login = datetime.strptime(user_last_login, '%Y-%m-%d %H:%M:%f').strftime('%m/%d/%Y')
-    # join_date
-    user_join_date = profile.query.filter_by(username=username).first().join_date
-    user_join_date = str(user_join_date)
-    # convert to human readable
-    # convert to %m/%d/%Y
-    user_join_date = datetime.strptime(user_join_date, '%Y-%m-%d %H:%M:%f').strftime('%m/%d/%Y')
-    # format post Dates to human readable
-    for i in range(len(post_date)):
-        post_date[i] = datetime.strptime(post_date[i][0], '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
-        #post_date[i] = datetime.strptime(post_date[i][0], '%Y-%m-%d %H:%M:%S.%f').strftime('%m/%d/%Y')
-        p_date = post_date[i]
-        #p_date = datetime.strptime(post_date[i], '%Y-%m-%d').strftime('%m/%d/%Y')
-    # return user profile
-    return render_template('profile.html', user_name=user_name, username=username, user_location=user_location,
-                            user_post_count=user_post_count, user_total_views=user_total_views,
-                            posts=user_post, user_last_login=user_last_login, date=p_date, user_join_date=user_join_date)
+
+
 
 
 
