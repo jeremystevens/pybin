b2a7d6af26e20d41030da57b93a41b7bad9c977c
jeremystevens <jeremiahstevens@gmail.com>
2022-06-13 19:15:56 -0700

    updated current version

--- application.py
+++ application.py
@@ -19,7 +19,7 @@
 # LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-__version__ = '1.1.4'
+__version__ = '1.1.6-HotFix' # current version
 
 # ============================================================
 """ application.py: - the main pybin flask server """
